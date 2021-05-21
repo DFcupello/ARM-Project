cbb2dd5e3aac1cf524192dc7f00816a7416c47c5
dc1020 <dc1020@ic.ac.uk>
2021-05-21 15:06:57 +0100

    Implemented binary loader and utilities functions. Worked: DC, AS

--- src/emulate.c
+++ src/emulate.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <math.h>
+#include <assert.h>
+
 #define MEM_SIZE 65536
 #define REG_SIZE 17
 
 uint32_t data[MEM_SIZE];
 uint32_t registers[REG_SIZE];
+enum inst{Data, Mul, Branch, Transfer};
 
-void binaryLoader(FILE *fptr, char *file);
+void binaryLoader(FILE *fptr, char *file, bool endian);
+uint32_t getByte(uint32_t word,int byte);
+uint32_t getNibble(uint32_t word, int n);
 
 int main(int argc, char *argv[]) {
   
@@ -19,31 +25,73 @@ int main(int argc, char *argv[]) {
     fptr = fopen(file, "rb");
   }
   if (fptr != NULL) {
-    binaryLoader(fptr, file);
+    binaryLoader(fptr, file, true);
     return EXIT_SUCCESS;
   }
   printf("Empty!\n");
   return 0;
 }
 
-// Loads the binary file
-void binaryLoader(FILE *fptr, char *file) {
-
-  unsigned char c;
-  unsigned char buffer[10];
-  
-  fread(buffer, sizeof(buffer),1, fptr);
-
-  for(int i = 0; i < 10; i++) {
-    printf("0x%x ", buffer[i]);
+/* 
+Takes a binary file pointer, file, and the bool endian, which if 1,
+stores the binary file in arm memory in uint32_t format,
+in little endian, and big endian if 0.
+*/
+void binaryLoader(FILE *fptr, char *file, bool endian) {
+  unsigned char buffer[4];
+  int i = 0;
+  while(i < MEM_SIZE) {
+    fread(buffer, sizeof(buffer), 1, fptr);
+    printf("----\n");
+    if (feof(fptr)) {
+      data[i] = 0;
+      break;
+    }
+    uint32_t word = 0;
+    for (int j = 0; j < 4; j++) {
+      printf("%d\n", buffer[j]);
+      word = word + pow(2, 8 * (endian ? (3 - j) : j)) * buffer[j];
+    }
+    data[i] = word;
+    i++;
   }
-
-  printf("\n");
   fclose(fptr);
 }
 
-// Parse the binary file
-void parseBinaryFile(void) {
+/* 
+Gets nth byte (0 is LSB, 3 is MSB)
+precondition: n is of range 0-3
+postcondition: r = nth byte
+*/
+
+uint32_t getByte(uint32_t word,int n) {
+  assert (n >= 0 && n <=3);  
+  uint32_t mask = 255;
+  uint32_t answer = 0;
+  mask = mask << 8 * n;
+  answer = mask & word;
+  answer = answer >> 8 * n;
+  return answer;
+}
+
+/* 
+  Gets nth nibble (0 is LSN, 7 is MSN)
+  precondition: n is of range 0-7
+  postcondition: r = nth nibble
+*/
+
+uint32_t getNibble(uint32_t word, int n) {
+  assert (n >= 0 && n <= 7);  
+  uint32_t mask = 255;
+  uint32_t answer = 0;
+  mask = mask << 4 * n;
+  answer = mask & word;
+  answer = answer >> 4 * n;
+  return answer;
+}
+
+// convert to Binary 
+void intToBinary(void) {
 
 }
 
@@ -51,6 +99,8 @@ void parseBinaryFile(void) {
 void distinguishInstructions(void) {
 
 }
+
+
 
 
 
