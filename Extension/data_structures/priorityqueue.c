5f4ec9b8147f7fc572e523a5bad0088ccd8edfac
dc1020 <dc1020@ic.ac.uk>
2021-06-12 15:01:42 +0100

    Finish implementing some commands for the debugger, including implementation of priority queue. Worked: DC, AS, NC

---
+++ Extension/data_structures/priorityqueue.c
@@ -0,0 +1,134 @@
+#include "priorityqueue.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+
+/*
+	Creates initial empty collection
+	Returns the pointer to its struct
+*/
+priorityQueue *allocatePriorityQueue(void)
+{
+	priorityQueue *emptyCollection = malloc(sizeof(priorityQueue));
+	emptyCollection->head = NULL;
+	emptyCollection->rear = NULL;
+	return emptyCollection;
+}
+
+/*
+	Takes the pointer to the collection, where to add new node, and node 32-bit int address.
+	Modifies the collection by adding the node to the REAR.
+*/
+void addNode(priorityQueue *collection, int val)
+{
+	node *newNode = malloc(sizeof(node));
+	newNode->value = val;
+	newNode->next = NULL;
+
+    if (collection->head == NULL) { // Empty collection case
+		assert(collection->rear == NULL);
+		collection->head = collection->rear = newNode;
+		return;
+	}
+	// Non-empty collection case
+    node *current = collection->head;
+    node *previous = NULL;
+    while (current && newNode->value > current->value) {
+        previous = current;
+		current = current->next;
+    }
+	if (current != NULL && current->value == val) {
+		free(newNode);
+		return;
+	}
+	if (previous == NULL) { // head case
+        newNode->next = current;
+        collection->head = newNode;
+        return;
+    }
+    if (current == NULL) { // rear case 
+        (collection->rear)->next = newNode;
+	    collection->rear = newNode;
+        return;
+    }
+    previous->next = newNode;
+    newNode->next = current;
+}
+
+/*
+	Prints the node contents in the form (value);
+*/
+void printNode(node *node) {
+	printf("(%d)", node->value);
+}
+
+/*
+	Prints the collection
+*/
+void printPriorityQueue(priorityQueue *collection)
+{
+	node *current = collection->head;
+	if (current == NULL)
+	{
+		printf("collection is empty\n");
+		return;
+	}
+	while (current != NULL)
+	{
+		printNode(current);
+		current = current->next;
+	}
+	printf("\n");
+}
+
+
+/*
+	Takes the pointer to the collection to take the value from.
+	Returns NULL the value of the head and then frees that head
+	PRE: collection is non empty
+*/
+int popMin(priorityQueue *collection)
+{
+	assert(collection->head != NULL);
+	node *oldHead = collection->head;
+	int res = oldHead->value;
+
+	if (collection->head == collection->rear)
+	{
+		collection->head = collection->rear = NULL;
+	}
+	else
+	{
+		collection->head = (collection->head)->next;
+	}
+	free(oldHead);
+	return res;
+}
+
+/*
+    Returns true if found.
+*/
+bool contains(priorityQueue *collection, int value) {
+    node *current = collection->head;
+    for (;current && current->value < value; current = current->next);
+    return (current != NULL) && current->value == value;
+}
+
+/*
+	Takes the pointer to the collection.
+	Frees every node which is still stored in the collection and then frees the collection itself
+*/
+void freePriorityQueue(priorityQueue *collection)
+{
+	node *current = collection->head;
+	while (current != NULL)
+	{
+		node *nodeToFree = current;
+		current = current->next;
+		free(nodeToFree);
+	}
+	free(collection);
+}
--- Extension/debugger.c
+++ Extension/debugger.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
+#include <ctype.h>
 #include "emulate.h"
 #include "utilities.h"
 #include "assemble.h"
 #include "inassembler.h"
 #include "inhandler.h"
+#include "priorityqueue.h"
 
 #define MAX_COMMAND_SIZE 551
 
@@ -40,7 +43,7 @@ Returns an int, representing the ENUM type Command
 */
 int lexerCommand(char **tokens, int tokenSize) {
     assert(tokenSize >= 1 && tokenSize <= 2);
-    if (tokenSize == 1 || tokenSize == 2) {
+    if (tokenSize == 1) {
         if (strlen(tokens[0]) == 1) {
             switch (tokens[0][0]) {
                 case 'q': return QUIT;
@@ -51,6 +54,15 @@ int lexerCommand(char **tokens, int tokenSize) {
                 break;
                 case 'h': return HELP;
                 break;
+                case 'p': return PRINT;
+                break;
+                default: return TOKENERR;
+            }
+        }
+    }
+    else if (tokenSize == 2) {
+        if (strlen(tokens[0]) == 1) {
+            switch(tokens[0][0]) {
                 case 'b': return BREAK;
                 break;
                 case 'w': return WATCH;
@@ -64,32 +76,63 @@ int lexerCommand(char **tokens, int tokenSize) {
     return TOKENERR;
 }
 
+char **commandTokenizer(char *string, int tokenSize) {
+    char *saveptr1 = string;
+    char **tokens = malloc(sizeof(char *) * tokenSize);
+    for (int i = 0; i < tokenSize; i++) {
+        char *temp =  strtok_r(saveptr1, " \n", &saveptr1);
+        tokens[i] = strcpy(malloc(strlen(temp) + 1), temp);
+    }
+    
+    return tokens;
+}
 /*
 Prints the command symbol > and waits for user input. Tries to parse result, by tokenising.
 */
-int readDebuggerCommand(void) {
+char **readDebuggerCommand(command *cmd, int *tokenSize) {
     printf("> ");
     char answer[MAX_COMMAND_SIZE];
-    scanf("%s", answer);
-    int tokenSize = getTokenSize(answer);
-    char *tokens[tokenSize];
-    tokenizer(answer, tokenSize, tokens);
-    return lexerCommand(tokens, tokenSize);
+    fgets(answer, MAX_COMMAND_SIZE, stdin);
+    if (strcmp(answer, "\n") == 0) {
+        *cmd = TOKENERR;
+        return NULL;
+    }
+    else {
+        *tokenSize = getTokenSize(answer);
+        char **tokens = commandTokenizer(answer, *tokenSize);
+        *cmd = lexerCommand(tokens, *tokenSize);
+        return tokens;
+    } 
 }
 
 void printHelp(void) {
     printf("List Of Commands:\n");
-    printf("q - quit the program.\n");
-    printf("n - Execute current line and print next.\n");
-    printf("r - runs the program until it notices a breakpoint.\n");
+    printf("q              - quit the program.\n");
+    printf("n              - Execute current line and print next.\n");
+    printf("r              - runs the program until it notices a breakpoint.\n");
     printf("w R{register#} - sets a watchpoint for the given register.\n");
-    printf("b {Line#} - sets a breakpoint for the given line.\n");
-    printf("p {address} - prints the content of the address.\n");
+    printf("b {Line#}      - sets a breakpoint for the given line.\n");
+    printf("p {address}    - prints the content of the address.\n");
     printf("p R{register#} - prints the content of the register.\n");
 }
 
+void freeTokens(char **tokens, int tokenSize) {
+    for (int i = 0; i < tokenSize; i++) {
+        free(tokens[i]);
+    }
+    free(tokens);
+}
 
-void debug(uint32_t data[], int instrCount, FILE *fptrAssembly) {
+void breakWatchFunc(priorityQueue *collection, char **tokens, int instrCount) {
+    int lineNum = atoi(tokens[1]);
+    if (lineNum >= 1 && lineNum <= instrCount)
+    {
+        addNode(collection, lineNum - 1);
+        printPriorityQueue(collection);
+    }
+}
+
+void debug(uint32_t data[], int instrCount, char assemblyInstrs[][MAX_COMMAND_SIZE]) {
     printf("WELCOME TO THE ARM DEBUGGER - press h to see list of commands.\n");
     uint32_t instructions[instrCount + 1];
     uint32_t registers[REG_SIZE] = {0};
@@ -101,35 +144,72 @@ void debug(uint32_t data[], int instrCount, FILE *fptrAssembly) {
     //fetch - decode - execute
     uint32_t pipeline[] = {-1, -1};
     // Preperation for debugging
-    int breakpoints[instrCount];
-    int watchpoints[instrCount];
+    priorityQueue *breakpoints = allocatePriorityQueue();
+    priorityQueue *watchpoints = allocatePriorityQueue();
     command cmd;
+    bool printLine = true;
     while (true) {
-        cmd = readDebuggerCommand();
-        printf("%d\n", cmd);
+        int tokenSize;
+        if (registers[PC] / 4 >= instrCount) {
+            break;
+        }
+        if (printLine) {
+            printf("%d %s\n", registers[PC] / 4 + 1, assemblyInstrs[registers[PC] / 4]);
+        }
+        char **tokens = readDebuggerCommand(&cmd, &tokenSize);
+        if (tokens == NULL) {
+            printf("Put a command :)\n");
+            continue;
+        }
         if (cmd == QUIT) {
+            freeTokens(tokens, tokenSize);
             break;
         }
         else if (cmd == TOKENERR) {
             printf("Invalid command :)\n");
+            freeTokens(tokens, tokenSize);
             continue;
         }
         switch (cmd) {
-        case RUN:
+        case RUN: 
+            {
+                bool notDone = true;
+                while (notDone) {
+                    notDone = emulateInstruction(data, registers, instructions, pipeline);
+                    notDone = notDone && !contains(breakpoints, registers[PC] / 4);
+                }
+                printLine = true;
+            }
         break;
         case NEXT:
             emulateInstruction(data, registers, instructions, pipeline);
+            printLine = true;
         break;
-        case HELP: printHelp();
+        case HELP: 
+            printHelp();
+            printLine = false;
         break;
         case BREAK:
+            { 
+                breakWatchFunc(breakpoints, tokens, instrCount);
+                printLine = false;
+            }
         break;
-        case PRINT: 
+        case PRINT: printLine = false;
         break;
         case WATCH:
+            { 
+                breakWatchFunc(watchpoints, tokens, instrCount);
+                printLine = false;
+            }
         break;
-        default: printf("Invalid command :)\n");
+        default: 
+            {
+                printf("Invalid command :)\n");
+                printLine = false;
+            }
         }
+        freeTokens(tokens, tokenSize);
     } 
     
     //while (emulateInstruction(data, registers, instructions, pipeline));
@@ -140,25 +220,31 @@ void debug(uint32_t data[], int instrCount, FILE *fptrAssembly) {
 int main(int argc, char *argv[]) {
 
     uint32_t data[MEM_SIZE] = {0};
-    char *binaryFile;
-    char *assemblyFile;
-    FILE *fptrBinary = NULL;
-    FILE *fptrAssembly = NULL;
+
+    char *binaryFile = "/homes/dc1020/arm11_testsuite/test_cases/add01";
+    char *assemblyFile = "/homes/dc1020/arm11_testsuite/test_cases/add01.s";
+    FILE *fptrBinary = fopen(binaryFile, "rb"); 
+    FILE *fptrAssembly = fopen(assemblyFile, "r");
     int instrCount = 0;
-    if (argc != 3) {
-        printf("Usage: ./debugger {Binary File} {Assembly File}\n");
-        return EXIT_FAILURE;
-    }
-    if (argv[1] != NULL && argv[2] != NULL) {
-        assemblyFile = argv[1];
-        binaryFile = argv[2];
-        fptrAssembly = fopen(assemblyFile, "r");
-        fptrBinary = fopen(binaryFile, "rb");
-    }
+    // if (argc != 3) {
+    //     printf("Usage: ./debugger {Binary File} {Assembly File}\n");
+    //     return EXIT_FAILURE;
+    // }
+    // if (argv[1] != NULL && argv[2] != NULL) {
+    //     assemblyFile = argv[1];
+    //     binaryFile = argv[2];
+    //     fptrAssembly = fopen(assemblyFile, "r");
+    //     fptrBinary = fopen(binaryFile, "rb");
+    // }
     if (fptrAssembly != NULL && fptrBinary != NULL) {
         // Preperation to emulate
         binaryLoader(fptrBinary, binaryFile, data, MEM_SIZE, &instrCount);
-        debug(data, instrCount);
+        char assemblyInstrs[instrCount][MAX_COMMAND_SIZE];
+        for (int i = 0; i < instrCount; i++) {
+            fgets(assemblyInstrs[i], MAX_COMMAND_SIZE, fptrAssembly);
+            strtok(assemblyInstrs[i], "\n");
+        }
+        debug(data, instrCount, assemblyInstrs);
         fclose(fptrAssembly);
         return EXIT_SUCCESS;
     }
